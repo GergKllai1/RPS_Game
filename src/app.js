7562a83bad354b35e8b95d9c716d647f71cb0288
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-18 20:50:54 +0100

    adds main rps code

--- src/app.js
+++ src/app.js
@@ -17,7 +17,32 @@ const rockPaperScissor = document.addEventListener('DOMContentLoaded', () => {
             ai.innerHTML = "Scissor"
         }
     };
-    test.addEventListener('click', () => {
+    const evaluate = () => {
+        if(choice.innerHTML === ai.innerHTML){
+            result.innerHTML = 'The game is a tie'
+        } else if(choice.innerHTML === 'Rock' && ai.innerHTML === 'Scissor'){
+            result.innerHTML = 'You have won'
+        } else if(choice.innerHTML === 'Scissor' && ai.innerHTML === 'Paper'){
+            result.innerHTML = 'You have won'
+        } else if(choice.innerHTML === 'Paper' && ai.innerHTML === 'Rock'){
+            result.innerHTML = 'You have won'
+        } else{
+            result.innerHTML = 'You have lost'
+        }
+    };
+    rock.addEventListener('click', () => {
         randomizer();
-    })
+        choice.innerHTML = "Rock"
+        evaluate()
+    });
+    paper.addEventListener('click', () => {
+        randomizer();
+        choice.innerHTML = "Paper"
+        evaluate()
+    });
+    scissor.addEventListener('click', () => {
+        randomizer();
+        choice.innerHTML = "Scissor"
+        evaluate()
+    });
 })
